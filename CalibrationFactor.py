f106c195397a391c831731843f583f514dd5ee12
jostlinhell <eleanor.terry-welsh@wsu.edu>
2024-05-14 13:57:16 -0700

    updated documentation

--- CalibrationFactor.py
+++ CalibrationFactor.py
@@ -3,9 +3,88 @@ import datetime
 import numpy as np
 
 class CalibrationFactor: 
+    """
+    A class used to manage data collected for calculating a calibration factor. 
+
+    ...
+
+    Attributes
+    ----------
+    collection_date : string
+        The current date. 
+    cal_rot : float 
+        The rotation used to collect the calibration factor, in degrees. 
+    locksense : float
+        The sensitivity scale if the lock-in amplifier, in volts.
+        e.g. if collecting on the 200mV scale, this should be entered as 0.002
+    init_v : float
+        The initial voltage reading from the oscilloscope. 
+    init_v_err : float
+        The error in the initial voltage reading from the oscilloscope. 
+    fin_v : float
+        The final voltage reading from the oscilloscope. 
+    fin_v_err : float
+        The error in the final voltage reading from the oscilloscope. 
+    wl : float
+        The wavelength of the probe laser, in cm. 
+    ol : float
+        The length of the path of the laser through the cell, in cm. 
+    ot : float
+        The oven temperature, in Celcius.
+    lp : float
+        The laser power, as read from the laser control box. 
+    lt : float
+        The laser temperature, as read from the laser control box. 
+
+    Methods
+    -------
+    __init__(self, cal_rot, locksense, init_v, init_v_err, fin_v, fin_v_err, wl, ol, ot, lp, lt)
+        Creates a CalibrationFactor object.
+    """
     #constructor
     #creates a new calibration factor object
     def __init__(self, cal_rot, locksense, init_v, init_v_err, fin_v, fin_v_err, wl, ol, ot, lp, lt):
+        """
+        Creates a CalibrationFactor object.
+
+        Parameters
+        ----------
+        self : CalibrationFactor
+            the CalibrationFactor object to be created. 
+        cal_rot : float 
+            The rotation used to collect the calibration factor, in degrees. 
+        locksense : float
+            The sensitivity scale if the lock-in amplifier, in volts.
+            e.g. if collecting on the 200mV scale, this should be entered as 0.002
+        init_v : float
+            The initial voltage reading from the oscilloscope. 
+        init_v_err : float
+            The error in the initial voltage reading from the oscilloscope. 
+        fin_v : float
+            The final voltage reading from the oscilloscope. 
+        fin_v_err : float
+            The error in the final voltage reading from the oscilloscope. 
+        calibration_factor : 
+            The calibration to use for converting voltage data to rotation data. 
+        calibration_factor_err : 
+            The error in the calibration factor. 
+        wl : float
+            The wavelength of the probe laser, in cm. 
+        ol : float
+            The length of the path of the laser through the cell, in cm. 
+        ot : float
+            The oven temperature, in Celcius.
+        lp : float
+            The laser power, as read from the laser control box. 
+        lt : float
+            The laser temperature, as read from the laser control box. 
+
+        Returns
+        -------
+        CalibrationFactor
+            Returns a new CalibrationFactor object.  
+        """
+
         self.collection_date = str(datetime.datetime.now())
         
         self.cal_rotation = cal_rot
@@ -28,6 +107,27 @@ class CalibrationFactor:
 
 
     def calculateCalFactor(self, scale, v_i, v_f, angle):
+        """
+        Calculates the value of a CalibrationFactor object.
+
+        Parameters
+        ----------
+        self : CalibrationFactor
+            the CalibrationFactor object to be manipulated. 
+        scale : float
+        v_i : float
+            The initial voltage reading. 
+        v_f : float
+            The final voltage reading.
+        angle : float
+            The calibration angle in degrees. 
+
+        Returns
+        -------
+        convsersion_factor : float
+            Returns the value of the conversion factor.  
+        """
+
         #calculate scaling factor from lockin sensitivity
         scaled_init = (scale * v_i) / 10
         scaled_final = (scale * v_f) / 10
@@ -36,10 +136,43 @@ class CalibrationFactor:
         return convsersion_factor
     
     def calculateCalFactorError(self, scale, v_i_err, v_f_err):
+        """
+        Calculates the total error in the conversion factor. 
+
+        Parameters
+        ----------
+        self : CalibrationFactor
+            the CalibrationFactor object to be manipulated. 
+        scale : float
+        v_i_err : float
+            The initial voltage reading error. 
+        v_f_err : float
+            The final voltage reading error.
+        
+        Returns
+        -------
+        convsersion_factor : float
+            Returns the value of the conversion factor error.  
+        """
+
         total_V_err = scale * (abs(v_i_err) + abs(v_f_err))
         return total_V_err
     
     def calFPrepforJSON(self):
+        """
+        Formats the calibration factor data to be saved to a JSON file.
+
+        Parameters
+        ----------
+        self : CalibrationFactor
+            the CalibrationFactor object to be manipulated. 
+        
+        Returns
+        -------
+        calibrationInfoJSON : dictionary
+            A dictionary object to be saved to a JSON file.  
+        """
+
         calibrationInfoJSON = {
             "dateCollected": self.collection_date,
             "calibrationRotation": self.cal_rotation,
--- DataPoint.py
+++ DataPoint.py
@@ -1,20 +1,89 @@
 
 
 class DataPoint: 
-    #constructor
-    #creates a new datapoint with an x value, a y value, a y error, and units for x and y
-    #if no units are specified default is None
-    #if no y_err is specified default is 0
+    """
+    A class used construct a generic data point with numerical x and y values. 
+
+    ...
+
+    Attributes
+    ----------
+        x_val : float
+            The x value of the data point.   
+        y_val : float
+            The y value of the data point. 
+        y_err : float
+            The error in the y value. Defaults to 0. 
+
+    Methods
+    -------
+    __init__(self, x_val, y_val, y_err=0)
+        Creates a DataPoint object.
+    """
+     
     def __init__(self, x_val, y_val, y_err=0):
+        """
+        Creates a DataPoint object.
+
+        Parameters
+        ----------
+        self : DataPoint
+            the DataPoint object to be created. 
+        x_val : float
+            The x value of the data point.   
+        y_val : float
+            The y value of the data point. 
+        y_err : float
+            The error in the y value. Defaults to 0. 
+
+
+        Returns
+        -------
+        VoltageReading
+            Returns a new VoltageReading object.  
+        """
         self.x_val = x_val
         self.y_val = y_val
         self.y_err = y_err
 
     
 class VoltageReading:
+    """
+    A class used to record a voltage reading and the timestamp at which that reading was taken. 
 
-    #constructor
+    ...
+
+    Attributes
+    ----------
+        v : float
+            The voltage reading from the oscilloscope.   
+        tstamp : float
+            The timestamp for the reading.   
+
+    Methods
+    -------
+    __init__(self, v, tstamp)
+        Creates a VoltageReading object.
+    """
+
     def __init__(self, v, tstamp):
+        """
+        Creates a VoltageReading object.
+
+        Parameters
+        ----------
+        self : VoltageReading
+            the VoltageReading object to be created. 
+        v : float
+            The voltage reading from the oscilloscope.   
+        tstamp : float
+            The timestamp for the reading.   
+
+        Returns
+        -------
+        VoltageReading
+            Returns a new VoltageReading object.  
+        """
         self.voltage = v
         self.timestamp = tstamp
     
